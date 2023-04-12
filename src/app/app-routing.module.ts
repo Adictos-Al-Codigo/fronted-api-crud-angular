8455b30fcc78715cae51b752979fe14a96cddffe
Celorio2001 <jahir.celorio2001@gmail.com>
2023-04-12 18:57:00 -0500

    Login añadido

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './Vista/dashboard/dashboard.component';
+import { EditarComponent } from './Vista/editar/editar.component';
+import { LoginComponent } from './Vista/login/login.component';
+import { NuevoComponent } from './Vista/nuevo/nuevo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'Login', pathMatch: 'full'
+  },
+  {
+    path: 'Login',
+    component: LoginComponent
+  },
+  {
+    path: 'Dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'Nuevo',
+    component: NuevoComponent
+  },
+  {
+    path: 'Editar',
+    component: EditarComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [LoginComponent,DashboardComponent,NuevoComponent,EditarComponent ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Plantillas/header/header.component';
+import { FooterComponent } from './Plantillas/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
